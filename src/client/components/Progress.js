d5bff96129a349277dc4a2330c77765e5d5158e3
mosta <info@manonworld.de>
2021-09-30 12:03:56 +0200

    feat(polished-ui) Closes #16

--- src/client/components/Progress.js
+++ src/client/components/Progress.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Progress } from 'semantic-ui-react';
 
 const calculateConfidence = ( confidence ) => (
-    <Progress percent={confidence} 
-        success={confidence >= 70 ? true : false}
-        warning={confidence >= 30 && confidence < 70 ? true : false}
-        error={confidence >= 0 && confidence < 30 ? true : false}
+    <Progress color='blue' percent={confidence} 
+        // success={confidence >= 70 ? true : false}
+        // warning={confidence >= 30 && confidence < 70 ? true : false}
+        // error={confidence >= 0 && confidence < 30 ? true : false}
     >
         {confidence}%
     </Progress>
