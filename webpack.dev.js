304a9dae5f687492835612f6279e9f6bdad300bb
mosta <info@manonworld.de>
2021-10-02 16:21:13 +0200

    feat(add-service-worker-to-dev) Add service worker to the dev environment Closes #22

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -97,6 +98,7 @@ module.exports = {
             verbose: true,
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
